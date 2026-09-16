f93352f89d2239699536cdf0206850d801256c37
machxanht <machxanht@gmail.com>
2026-09-16 19:10:11 +0700

    feat(ui): switch web entry to pixel-perfect Stitch clone

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App'
-import { ReferenceHomeShell } from './ReferenceHomeShell'
-import './styles.css'
-import './premium-heritage.css'
-import './stitch-logo-theme.css'
-import './stitch-exact.css'
-import './uiux-pro-max-board.css'
-import './uiux-pro-max-home.css'
+import { PixelPerfectApp } from './PixelPerfectApp'
+import './pixel-perfect.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ReferenceHomeShell>
-      <App />
-    </ReferenceHomeShell>
+    <PixelPerfectApp />
   </React.StrictMode>,
 )
